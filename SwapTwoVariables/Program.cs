9b2ba3d08bbc1fcb3b54d103fd32889457e84778
iloozealot <iloozealot@gmail.com>
2023-11-16 21:47:29 -0600

    Add project files.

---
+++ SwapTwoVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+int? variableOne  = 5;
+int? variableTwo = 2;
+int? variableThree = null;
+
+//Console.WriteLine($"variableTwo is {variableOne} and variableOne is {variableTwo}.");
+
+variableThree = variableOne;
+variableOne = variableTwo;
+variableTwo = variableThree;
+
+Console.WriteLine($"{variableOne}, {variableTwo}");
+
